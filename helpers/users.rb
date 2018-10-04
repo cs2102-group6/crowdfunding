59331446aa070f0306cbfa52bb1966049d5ddbd1
shanwpf <shanwpf@gmail.com>
2018-10-05 01:04:10 +0800

    Modularise code and add sessions and authentication

---
+++ helpers/users.rb
@@ -0,0 +1,30 @@
+module Users
+    def create_user
+        # The params hash contains information in the post request
+        # params[:key] is the value where <input name="key"> in the view
+        email = params[:email]
+        password = params[:password]
+        first_name = params[:first_name]
+        last_name = params[:last_name]
+        $db.exec(
+            "INSERT INTO users
+             (email, password, first_name, last_name, is_admin)
+             VALUES (#{email}, #{password}, #{first_name}, #{last_name}, false);"
+        )
+    end
+
+    def find_user
+        $db.exec(
+            "SELECT * FROM users
+           WHERE email=#{params[:email]} AND password=#{params[:password]}"
+        )
+    end
+
+    def delete_user
+        $db.exec(
+            "DELETE FROM users
+            WHERE email=#{params[:email]}"
+        ) 
+    end
+end
+
---
+++ helpers/utils.rb
@@ -0,0 +1,23 @@
+module Utils
+    # Converts empty string hash values to NULL
+    # to prevent storing empty strings in db when there is no user input
+    # Surrounds the strings with inverted commas if they are not empty
+    def process_input(hash)
+        hash.each do |key, value|
+            if hash[key] == ''
+                hash[key] = 'NULL'
+            else
+                hash[key] = "'#{hash[key]}'"
+            end
+        end
+    end
+
+    # Checks if user is authenticated.
+    # Sends HTTP 401 Not Authorized if not authenticated.
+    # Call this method in routes that require the user to be authenticated.
+    def require_authenticated
+        if !session[:authenticated]
+            halt 401
+        end
+    end
+end
